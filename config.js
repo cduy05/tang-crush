9394d364ad16279b1820bd96a7ebfdba2ea895ff
cduy05 <86726802+cduy05@users.noreply.github.com>
2021-06-30 21:06:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Anh biết anh làm em buồn nhiều lần :<<',
     desc: 'Nhưng anh vẫn muốn ta có thể đi qua hết những năm tháng cấp 3 ',
-    btnYes: 'Vẫn yêu anh màa <33',
+    btnYes: 'Dạ đượcc <3',
     btnNo: 'Không, anh à :3',
     question:'Chúng ta có thể bỏ qua tất cả và yêu nhau như ngày đầu được khôngggg :<',
     btnReply: 'Gửi cho anh <3',
